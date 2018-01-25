3de5e829fd6668da07e6f93c10ccdd03f72a2c5b
gaozhihua.aerozhonghuan.com <vv11uhtfsMXT>
2018-01-25 21:03:15 +0800

    去掉无用引用

--- src/main/java/com/luck/controller/SimplyController.java
+++ src/main/java/com/luck/controller/SimplyController.java
@@ -2,7 +2,6 @@ package com.luck.controller;
 
 import com.luck.dto.CommonRespondDto;
 import com.luck.dto.SendStringDto;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
